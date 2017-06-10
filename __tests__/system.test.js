63f9065374ab8b87326bb21e8dd9897f53ced61c
George South <ecuageo@gmail.com>
2017-06-10 09:32:35 -0600

    multiple elevators

--- __tests__/system.test.js
+++ __tests__/system.test.js
@@ -4,3 +4,9 @@ test('createSystem returns a single elevator', () => {
   const system = createSystem(1, 1)
   expect(system.elevators).toEqual([{state: "CLOSED", floor: 1}])
 })
+
+test('createSystem returns a few elevators', () => {
+  const system = createSystem(3, 1)
+  expect(system.elevators).toEqual([{state: "CLOSED", floor: 1},{state: "CLOSED", floor: 1},{state: "CLOSED", floor: 1}])
+})
+
